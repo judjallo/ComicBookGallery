bb09e7d9e1420d1aef43822052d7c6e072dfe799
Elita Judjallo <elita@webreality.co.uk>
2019-05-02 09:40:10 +0100

    My first commit

---
+++ ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; } // Artist comes 2 model artist
+        public bool Favorite { get; set; } 
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + "NR:" + IssueNumber;
+            }
+        } 
+
+        //series-title-issuennumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
+
+    }
+}
